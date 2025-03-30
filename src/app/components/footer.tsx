0a603db7b8888a4d9879790b6ce0674d931566b3
juanmoreno96 <juanmoreno199614@gmail.com>
2025-03-30 15:24:36 -0500

    updated the footer and fix the scrolling

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,16 +1,20 @@
 const Footer = () => {
-    return (
-      <footer className="bg-rose-950/50 text-white py-6 mt-auto relative z-50">
-        <div className="container mx-auto text-center">
-          <p>&copy; {new Date().getFullYear()} Grizzly Bear Running Club. All rights reserved.</p>
-          <div className="mt-4">
-            <a href="https://www.facebook.com" className="text-blue-500 hover:underline mx-2">Facebook</a>
-            <a href="https://www.twitter.com" className="text-blue-400 hover:underline mx-2">Twitter</a>
-            <a href="https://www.instagram.com" className="text-pink-500 hover:underline mx-2">Instagram</a>
-          </div>
+  return (
+    <footer className="bg-rose-950/50 text-white py-6 mt-auto relative z-50">
+      <div className="container mx-auto text-center flex flex-col items-center">
+        {/* Copyright Section */}
+        <p>&copy; {new Date().getFullYear()} Grizzly Bear Running Club. All rights reserved.</p>
+        
+        {/* Social Links Section */}
+        <div className="flex space-x-4">
+          <a href="https://www.facebook.com" className="text-blue-500 hover:underline">Facebook</a>
+          <a href="https://www.twitter.com" className="text-blue-400 hover:underline">Twitter</a>
+          <a href="https://www.instagram.com" className="text-pink-500 hover:underline">Instagram</a>
         </div>
-      </footer>
-    );
-    }
-  
-  export default Footer;
+        <p>Build by people from Laredo Tx</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
